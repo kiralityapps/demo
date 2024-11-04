1f84648ff30933467e284cc758bdbeb8a44d4596
kiralityapps <giavarini@kirality.com>
2024-11-04 15:36:58 -0700

    Update OrderServices.kt

--- src/main/kotlin/com/amex_teksystems/demo/services/OrderServices.kt
+++ src/main/kotlin/com/amex_teksystems/demo/services/OrderServices.kt
@@ -1,11 +1,22 @@
-package com.amex_teksystems.demo
+package com.amex_teksystems.demo.services
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import com.amex_teksystems.demo.model.Item
+import com.amex_teksystems.demo.model.Order
 
-@SpringBootApplication
-class DemoApplication
+import org.springframework.stereotype.Service
 
-fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
-}
+
+@Service
+class OrderService {
+
+	privae val prices = mapOf("apple" to 0.60, "orage" to 0.25)
+	private val orders = mutableMapOf<Long, Order>()
+	private var currentId = 1L
+	
+	
+	fun createOrder(items: List<Item>): Order {
+		val totalCost = items.sumOf { (prices[it.name.lowercase()] ?: 0.0) * it.quantity }
+		val order = Order(currentId++, items, totalCost)
+		orders[order.id] = order
+		return order
+	}
