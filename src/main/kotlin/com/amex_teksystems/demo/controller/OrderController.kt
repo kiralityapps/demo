00b0e3ce25cc15ff4f50cf7aa205b70f31cf674d
kiralityapps <giavarini@kirality.com>
2024-11-04 15:24:05 -0700

    Update OrderController.kt

--- src/main/kotlin/com/amex_teksystems/demo/controller/OrderController.kt
+++ src/main/kotlin/com/amex_teksystems/demo/controller/OrderController.kt
@@ -1,4 +1,18 @@
 package com.amex_teksystems.demo.controller
 
+import com.amex_teksystems.demo.model.Item
+import com.amex_teksystems.demo.model.Order
 
+import org.springframework.http.ResponseEntit
+import org.springframework.web.bind.annotation.*
 
+@RestController
+@RequestMapping("/api/orders")
+class OrderController(private val orderService: OrderService) {
+
+		@PostMapping
+		fun createOrder (@RequestBody items: List<Item>): ResponseEntity<Order> {
+			val order = orderService.createOrder(items)
+			return ResponseEntity.ok(order)
+		}
+		
