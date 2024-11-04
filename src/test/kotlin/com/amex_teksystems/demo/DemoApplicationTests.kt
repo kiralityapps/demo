2cd5ac4beb9a5ffd56d8c009447b2df83031ede7
kiralityapps <giavarini@kirality.com>
2024-11-04 15:56:07 -0700

    Update DemoApplicationTests.kt
    initial unit tests: CreateOder, 0 Items, 1 Item

--- src/test/kotlin/com/amex_teksystems/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/amex_teksystems/demo/DemoApplicationTests.kt
@@ -1,13 +1,40 @@
 package com.amex_teksystems.demo
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired
+
+
+import com.amex_teksystems.demo.model.Item
+import com.amex_teksystems.demo.service.OrderServices
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	fun contextLoads() {
+	}
+	
+	@Test
+	fun 'test order create valid items'() {
+		val items = listOf(Item("apple", 2), Item("orange", 4))
+		val order = orderService.createOrder(items)
+		assertEquals(2.20, order.totalCost)
+	}
+	
+	@Test
+	fun 'test order create no items'() {
+		val items = emptyList<Item>()
+		val order = orderService.createOrder(items)
+		assertEquals(0.00, order.totalCost)
+	}
+	
+	@Test
+	fun 'test order create single fruit item'() {
+		val items = listOf(Item("apple", 1))
+		val order = orderService.createOrder(items)
+		assertEquals(0.60, order.totalCost)
 	}
 
 }
